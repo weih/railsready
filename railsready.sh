774b604f97a6810ad840f1645c01aba4d6f77da5
Joshua Frye <joshfng@gmail.com>
2011-09-10 09:44:23 -0400

    Save file to correct path.

--- railsready.sh
+++ railsready.sh
@@ -104,7 +104,7 @@ echo -e "\n=> Downloading and running recipe for $distro...\n"
 if [[ $MACHTYPE = *linux* ]] ; then
   wget --no-check-certificate -O $railsready_path/src/$distro.sh https://raw.github.com/joshfng/railsready/osx-support/recipes/$distro.sh && cd $railsready_path/src && bash $distro.sh $ruby_version $ruby_version_string $ruby_source_url $ruby_source_tar_name $ruby_source_dir_name $whichRuby $railsready_path $log_file
 else
-  curl -O $railsready_path/src/$distro.sh https://raw.github.com/joshfng/railsready/osx-support/recipes/$distro.sh && cd $railsready_path/src && bash $distro.sh $ruby_version $ruby_version_string $ruby_source_url $ruby_source_tar_name $ruby_source_dir_name $whichRuby $railsready_path $log_file
+  curl -o https://raw.github.com/joshfng/railsready/osx-support/recipes/$distro.sh $railsready_path/src/$distro.sh && cd $railsready_path/src && bash $distro.sh $ruby_version $ruby_version_string $ruby_source_url $ruby_source_tar_name $ruby_source_dir_name $whichRuby $railsready_path $log_file
 fi
 echo -e "\n==> done running $distro specific commands..."
 
