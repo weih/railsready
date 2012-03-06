e2e2afb8832e171fc4fd1b084ea90945dea47926
Josh Frye <joshfng@gmail.com>
2012-03-05 19:29:14 -0500

    Update to p125

--- railsready.sh
+++ railsready.sh
@@ -12,10 +12,10 @@ shopt -s nocaseglob
 set -e
 
 ruby_version="1.9.3"
-ruby_version_string="1.9.3-p0"
-ruby_source_url="http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p0.tar.gz"
-ruby_source_tar_name="ruby-1.9.3-p0.tar.gz"
-ruby_source_dir_name="ruby-1.9.3-p0"
+ruby_version_string="1.9.3-p125"
+ruby_source_url="http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p125.tar.gz"
+ruby_source_tar_name="ruby-1.9.3-p125.tar.gz"
+ruby_source_dir_name="ruby-1.9.3-p125"
 script_runner=$(whoami)
 railsready_path=$(cd && pwd)/railsready
 log_file="$railsready_path/install.log"
