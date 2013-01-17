952412823fa78a035a5ac67296049af268009d1b
Josh Frye <joshfng@gmail.com>
2013-01-17 12:50:35 -0500

    Update Ruby patch level

--- railsready.sh
+++ railsready.sh
@@ -12,10 +12,10 @@ shopt -s nocaseglob
 set -e
 
 ruby_version="1.9.3"
-ruby_version_string="1.9.3-p362"
-ruby_source_url="http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p362.tar.gz"
-ruby_source_tar_name="ruby-1.9.3-p362.tar.gz"
-ruby_source_dir_name="ruby-1.9.3-p362"
+ruby_version_string="1.9.3-p374"
+ruby_source_url="http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p374.tar.gz"
+ruby_source_tar_name="ruby-1.9.3-p374.tar.gz"
+ruby_source_dir_name="ruby-1.9.3-p374"
 script_runner=$(whoami)
 railsready_path=$(cd && pwd)/railsready
 log_file="$railsready_path/install.log"
