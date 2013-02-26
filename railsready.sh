2a592d3127c5a0fac3d0e2daa70d8c891678d1e1
Josh Frye <joshfng@gmail.com>
2013-02-26 15:26:41 -0500

    Update to Ruby 2.0.0-p0

--- railsready.sh
+++ railsready.sh
@@ -8,14 +8,15 @@
 # Contributions from: Wayne E. Seguin <wayneeseguin@gmail.com>
 # Contributions from: Ryan McGeary <ryan@mcgeary.org>
 #
+# http://ftp.ruby-lang.org/pub/ruby/2.0/ruby-2.0.0-p0.tar.gz
 shopt -s nocaseglob
 set -e
 
-ruby_version="1.9.3"
-ruby_version_string="1.9.3-p392"
-ruby_source_url="http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p392.tar.gz"
-ruby_source_tar_name="ruby-1.9.3-p392.tar.gz"
-ruby_source_dir_name="ruby-1.9.3-p392"
+ruby_version="2.0.0"
+ruby_version_string="2.0.0-p0"
+ruby_source_url="http://ftp.ruby-lang.org/pub/ruby/2.0/ruby-2.0.0-p0.tar.gz"
+ruby_source_tar_name="ruby-2.0.0-p0.tar.gz"
+ruby_source_dir_name="ruby-2.0.0-p0"
 script_runner=$(whoami)
 railsready_path=$(cd && pwd)/railsready
 log_file="$railsready_path/install.log"
