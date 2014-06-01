88c7489e22c4d4bcfd04211a243648a14ae358c3
Waldyr Souza <waldyr.ar@gmail.com>
2014-06-01 01:01:19 -0500

    Corrected URL for OSX due Github's update in user content domain

--- railsready.sh
+++ railsready.sh
@@ -104,7 +104,7 @@ echo -e "\n=> Downloading and running recipe for $distro...\n"
 if [[ $system_os = *linux* ]] ; then
   wget --no-check-certificate -O $railsready_path/src/$distro.sh https://raw.github.com/joshfng/railsready/master/recipes/$distro.sh && cd $railsready_path/src && bash $distro.sh $ruby_version $ruby_version_string $ruby_source_url $ruby_source_tar_name $ruby_source_dir_name $whichRuby $railsready_path $log_file
 else
-  cd $railsready_path/src && curl -O https://raw.github.com/joshfng/railsready/master/recipes/$distro.sh && bash $distro.sh $ruby_version $ruby_version_string $ruby_source_url $ruby_source_tar_name $ruby_source_dir_name $whichRuby $railsready_path $log_file
+  cd $railsready_path/src && curl -O https://raw.githubusercontent.com/joshfng/railsready/master/recipes/$distro.sh && bash $distro.sh $ruby_version $ruby_version_string $ruby_source_url $ruby_source_tar_name $ruby_source_dir_name $whichRuby $railsready_path $log_file
 fi
 echo -e "\n==> done running $distro specific commands..."
 
